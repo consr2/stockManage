64b6eedfe5120821f00f6e09abd250975d5581fc
consr <qqq4861@naver.com>
2026-02-17 22:52:55 +0900

    feat: 로그 추가

--- src/main/java/com/my/shop/main/MainController.java
+++ src/main/java/com/my/shop/main/MainController.java
@@ -1,13 +1,16 @@
 package com.my.shop.main;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MainController {
@@ -15,7 +18,8 @@ public class MainController {
     private final MainService mainService;
 
     @GetMapping("/index")
-    public String main(){
+    public String main(HttpServletRequest request){
+        log.info("유저 접속 : {}", request.getRemoteAddr());
         return "main/main";
     }
 
