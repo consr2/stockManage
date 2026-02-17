b0067c8b4a04fe6cf53f1a1bbe645560d879cb11
consr <qqq4861@naver.com>
2026-02-17 19:55:59 +0900

    fix: replace 오타 및 자동빌드 bat 추가

---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+set IMAGE_NAME=shop
+set TAG=latest
+
+echo [1/4] Spring Clean...
+call ./gradlew clean
+:: 만약 Maven을 사용한다면: call mvn clean
+
+echo [2/4] Spring Build...
+call ./gradlew bootJar
+:: 만약 Maven을 사용한다면: call mvn package
+
+echo [3/4] Docker Image Delete...
+docker rmi -f %IMAGE_NAME%:%TAG%
+
+echo [4/4] Docker Image Build...
+docker build -t %IMAGE_NAME%:%TAG% .
+
+echo Docker Run...
+docker run -d --name shop --network db -p 80:80 -e SPRING_PROFILES_ACTIVE=prop shop
+
+echo 모든 작업이 완료되었습니다!
+pause
