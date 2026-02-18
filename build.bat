2523e9f739ba9e36aa9c297192ad64c97a790fef
consr <qqq4861@naver.com>
2026-02-18 14:55:49 +0900

    feat: 설정 수정 및 자잘한 버그 수정

--- build.bat
+++ build.bat
@@ -17,7 +17,7 @@ echo [4/4] Docker Image Build...
 docker build -t %IMAGE_NAME%:%TAG% .
 
 echo Docker Run...
-docker run -d --name shop --network db -p 80:80 -v D:/log:/app/logs -e SPRING_PROFILES_ACTIVE=prop  shop
+docker run -d --name shop --network db -p 80:80-e SPRING_PROFILES_ACTIVE=prop  shop
 
 echo 모든 작업이 완료되었습니다!
 pause
