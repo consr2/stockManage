b2410e6150511bd0b7fc0b02a21611451da85cce
consr <qqq4861@naver.com>
2026-02-25 20:41:54 +0900

    DDL수정

--- build.bat
+++ build.bat
@@ -2,22 +2,26 @@
 set IMAGE_NAME=shop
 set TAG=latest
 
-echo [1/4] Spring Clean...
+echo [1/5] Stop and Remove Container...
+docker stop shop 2>nul
+docker rm shop 2>nul
+
+echo [2/5] Spring Clean...
 call ./gradlew clean
 
-
-echo [2/4] Spring Build...
+echo [3/5] Spring Build...
 call ./gradlew bootJar
 
+echo [4/5] Docker Image Delete...
+docker rmi -f %IMAGE_NAME%:%TAG% 2>nul
 
-echo [3/4] Docker Image Delete...
-docker rmi -f %IMAGE_NAME%:%TAG%
-
-echo [4/4] Docker Image Build...
+echo [5/5] Docker Image Build...
 docker build -t %IMAGE_NAME%:%TAG% .
 
 echo Docker Run...
-docker run -d --name shop --network db -p 80:80 -e SPRING_PROFILES_ACTIVE=prop -e TZ=Asia/Seoul  shop
+docker run -d --name shop --network db -p 80:80 -e SPRING_PROFILES_ACTIVE=prop -e TZ=Asia/Seoul %IMAGE_NAME%
 
 echo 모든 작업이 완료되었습니다!
 pause
+
+
